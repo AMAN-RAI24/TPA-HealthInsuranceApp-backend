b0e48365282d33264ec87dad748c13cce4906a75
Prahlad Bhat <kpbhat@deloitte.com>
2022-05-19 14:01:03 +0530

    file-upload test

--- src/main/java/com/insurecorp/insureCorp/controlllers/Test.java
+++ src/main/java/com/insurecorp/insureCorp/controlllers/Test.java
@@ -1,11 +1,25 @@
 package com.insurecorp.insureCorp.controlllers;
 
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.insurecorp.insureCorp.entities.User;
 import com.insurecorp.insureCorp.repositories.UserRepository;
+import com.insurecorp.insureCorp.requestModels.TestFileUploadRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 @RestController
 public class Test {
@@ -23,5 +37,24 @@ public class Test {
     @GetMapping("/get/{id}")
     User getUser(@PathVariable Integer id){
         return userRepository.findById(id).get();
+    }
+    @PostMapping(value = "/test-file",consumes = MediaType.ALL_VALUE)
+    public void addFile(@RequestBody MultipartFile file) throws IOException {
+        String projectId="us-gcp-ame-con-116-npd-1";
+        String bucketName="hu-may-prod-insure-corp";
+        System.out.println("dsfsadf");
+        System.out.println(file.getOriginalFilename());
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+        BlobId blobId = BlobId.of(bucketName, "uploaded_image");
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+        storage.create(blobInfo, file.getBytes());
+        System.out.println(
+                "File " + file.getOriginalFilename() + " uploaded to bucket " + bucketName + " as " + "uploaded_image");
+
+//        System.out.println(file.getName());
+
+//        System.out.println(fileDetails.getObjectName());
+//        fileDetails.setFile(new File("./a"));
+//        System.out.println(fileDetails.getFile().getName());
     }
 }
